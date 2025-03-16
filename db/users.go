5e7d5c0ff1ee52f3eed61b0f18f3d1c21ec0a667
thehxdev <hossein.khosravi.ce@gmail.com>
2025-03-16 14:27:59 +0330

    implemented a `userCache` for faster user authentication
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- db/users.go
+++ db/users.go
@@ -1,32 +1,86 @@
 package db
 
+import (
+	"sync"
+)
+
 type User struct {
-	UserId    int
-	FirstName string
-	Username  string
-	IsAdmin   bool
+	UserId  int
+	IsAdmin bool
+}
+
+var userCache = &struct {
+	mu   *sync.RWMutex
+	data map[int]bool
+}{
+	mu:   &sync.RWMutex{},
+	data: make(map[int]bool),
 }
 
 func (db *DB) UserInsert(user User) error {
-	stmt := `INSERT INTO users (user_id, first_name, username, is_admin) VALUES (?, ?, ?, ?)`
-	_, err := db.Write.Exec(stmt, user.UserId, user.FirstName, user.Username, user.IsAdmin)
-	return err
+	stmt := `INSERT INTO users (user_id, is_admin) VALUES (?, ?)`
+	_, err := db.Write.Exec(stmt, user.UserId, user.IsAdmin)
+	if err != nil {
+		return err
+	}
+	userCache.mu.Lock()
+	if _, ok := userCache.data[user.UserId]; !ok {
+		userCache.data[user.UserId] = user.IsAdmin
+	}
+	userCache.mu.Unlock()
+	return nil
 }
 
-func (db *DB) UserSetAdmin(userId int) error {
-	stmt := `UPDATE users SET IsAdmin = 1 WHERE user_id = ?`
-	_, err := db.Write.Exec(stmt, userId)
-	return err
+func (db *DB) UserUpdateAdminStat(userId int, isAdmin bool) error {
+	stmt := `UPDATE users SET IsAdmin = ? WHERE user_id = ?`
+	_, err := db.Write.Exec(stmt, isAdmin, userId)
+	if err != nil {
+		return err
+	}
+	userCache.mu.Lock()
+	if _, ok := userCache.data[userId]; ok {
+		userCache.data[userId] = isAdmin
+	}
+	userCache.mu.Unlock()
+	return nil
 }
 
 func (db *DB) UserAuthenticate(userId int) (*User, error) {
 	u := &User{
 		UserId: userId,
 	}
+
+	userCache.mu.RLock()
+	if isAdmin, ok := userCache.data[userId]; ok {
+		u.IsAdmin = isAdmin
+		userCache.mu.RUnlock()
+		return u, nil
+	}
+	userCache.mu.RUnlock()
+
 	stmt := `SELECT is_admin FROM users WHERE user_id = ?`
 	err := db.Read.QueryRow(stmt, userId).Scan(&u.IsAdmin)
 	if err != nil {
-		u = nil
+		return nil, err
 	}
-	return u, err
+
+	userCache.mu.Lock()
+	userCache.data[userId] = u.IsAdmin
+	userCache.mu.Unlock()
+
+	return u, nil
+}
+
+func (db *DB) UserDelete(userId int) error {
+	stmt := `DELETE FROM users WHERE user_id = ?`
+	_, err := db.Read.Exec(stmt, userId)
+	if err != nil {
+		return err
+	}
+	userCache.mu.Lock()
+	if _, ok := userCache.data[userId]; ok {
+		delete(userCache.data, userId)
+	}
+	userCache.mu.Unlock()
+	return nil
 }

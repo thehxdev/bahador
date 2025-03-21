5e240b5abf31800b03c7b717b6e56c9ce98cbcc3
thehxdev <hossein.khosravi.ce@gmail.com>
2025-03-21 18:35:39 +0330

    implemented error types for file upload process
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

---
+++ cmd/bahador/errors.go
@@ -0,0 +1,32 @@
+package main
+
+type EmptyFileNameError struct{}
+
+func (e *EmptyFileNameError) Error() string {
+	return "file name is empty"
+}
+
+type MaxFileSizeError struct{}
+
+func (e *MaxFileSizeError) Error() string {
+	return "file size is bigger than maximum file size"
+}
+
+type IncompleteDownloadError struct{}
+
+func (e *IncompleteDownloadError) Error() string {
+	return "file download is incomplete"
+}
+
+type NonZeroStatusError struct{}
+
+func (e *NonZeroStatusError) Error() string {
+	return "non-zero http response status code"
+}
+
+var (
+	ErrEmptyFileName = &EmptyFileNameError{}
+	ErrMaxFileSize   = &MaxFileSizeError{}
+	ErrIncompleteDownload = &IncompleteDownloadError{}
+	ErrNonZeroStatusCode  = &NonZeroStatusError{}
+)

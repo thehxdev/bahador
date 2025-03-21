5eed6b53e9daee6a3b23da8ca5b90c079a45f5d4
thehxdev <hossein.khosravi.ce@gmail.com>
2025-03-21 18:37:46 +0330

    implemented file upload through pipes
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- cmd/bahador/app.go
+++ cmd/bahador/app.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"context"
-	"errors"
 	"io"
 	"log"
+	"mime"
 	"net/http"
 	"os"
 	"path"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/thehxdev/bahador/db"
@@ -19,7 +18,7 @@ import (
 )
 
 const (
-	maxFileSize  int    = 500 * 1024 * 1024
+	maxFileSize  int64  = 210 * 1024 * 1024
 	workersCount int    = 5
 	tokenEnvVar  string = "BAHADOR_BOT_TOKEN"
 	hostEnvVar   string = "BAHADOR_BOT_HOST"
@@ -34,13 +33,13 @@ type dlResult struct {
 }
 
 type dlJob struct {
-	url string
+	url     string
 	resChan chan dlResult
 }
 
 type dlFile struct {
 	name string
-	size int
+	size int64
 }
 
 type App struct {
@@ -69,19 +68,20 @@ func AppNew(ctx context.Context) (*App, error) {
 	}
 
 	a := &App{
-		DB:  db,
-		Log: log.New(os.Stderr, "[bahador] ", log.Ldate|log.Lshortfile),
+		DB:      db,
+		Log:     log.New(os.Stderr, "[bahador] ", log.Ldate|log.Lshortfile),
 		jobChan: make(chan dlJob, workersCount),
 	}
 
 	for range workersCount {
-		go a.worker(ctx)
+		// go a.worker(ctx)
+		go a.workerWithPipe(ctx)
 	}
 
 	return a, nil
 }
 
-func (app *App) worker(ctx context.Context) {
+func (app *App) workerWithPipe(ctx context.Context) {
 	for {
 		select {
 		case <-ctx.Done():
@@ -90,43 +90,143 @@ func (app *App) worker(ctx context.Context) {
 		}
 
 		job := <-app.jobChan
-		func() {
-			res := dlResult{}
-			defer func() { job.resChan <- res }()
+		res := dlResult{}
 
-			dlFile, err := app.downloadAndSaveFile(ctx, job.url)
+		res.error = func() error {
+			jobCtx, jobCancel := context.WithTimeout(ctx, time.Minute*30)
+			defer jobCancel()
+
+			fname, fsize, err := getRemoteFileInfo(jobCtx, job.url)
 			if err != nil {
-				res.error = err
-				return
+				return err
 			}
-			defer os.Remove(dlFile.name)
+			if fname == "" {
+				return ErrEmptyFileName
+			}
+			if fsize > maxFileSize {
+				return ErrMaxFileSize
+			}
 
-			app.Log.Println("file downloaded:", dlFile.name)
-			app.Log.Println("uploading file:", dlFile.name)
-
-			msg, err := app.UploadFile(dlFile.name, dlFile.name)
+			dlReq, err := http.NewRequestWithContext(jobCtx, "GET", job.url, nil)
 			if err != nil {
-				res.error = err
-				return
+				return err
 			}
-			res.msg = msg
+
+			dlResp, err := http.DefaultClient.Do(dlReq)
+			if err != nil {
+				return err
+			}
+			defer dlResp.Body.Close()
+
+			if dlResp.StatusCode != http.StatusOK {
+				return ErrNonZeroStatusCode
+			}
+
+			errChan := make(chan error, 2)
+			pipeReader, pipeWriter := io.Pipe()
+			defer func() {
+				pipeWriter.Close()
+				pipeReader.Close()
+			}()
+
+			go func() {
+				n, err := io.Copy(pipeWriter, dlResp.Body)
+				if err != nil {
+					goto ret
+				}
+				if n != fsize {
+					err = ErrIncompleteDownload
+				}
+			ret:
+				pipeWriter.CloseWithError(err)
+				errChan <- err
+			}()
+
+			go func() {
+				uparams := telbot.UploadParams{
+					ChatId: app.Bot.Self.Id,
+					Method: "sendDocument",
+				}
+				msg, err := app.Bot.UploadFile(jobCtx, uparams, telbot.FileReader{
+					Reader:   pipeReader,
+					Kind:     "document",
+					FileName: fname,
+				})
+				if err != nil {
+					pipeReader.CloseWithError(err)
+				}
+				res.msg = msg
+				errChan <- err
+			}()
+
+			for range 2 {
+				select {
+				case err = <-errChan:
+					if err != nil {
+						return err
+					}
+				case <-jobCtx.Done():
+					return jobCtx.Err()
+				}
+			}
+
+			return nil
 		}()
-	}
-}
 
-func getFilename(resp *http.Response) string {
-	cd := resp.Header.Get("Content-Disposition")
-	if cd != "" {
-		_, fname, found := strings.Cut(cd, "filename=")
-		if !found {
-			return ""
+		if res.error == nil && res.msg != nil && res.msg.Document != nil {
 		}
-		return strings.Fields(fname)[0]
+
+		job.resChan <- res
 	}
-	return path.Base(resp.Request.URL.Path)
 }
 
-func getRemoteFileInfo(ctx context.Context, fileUrl string) (fname string, fsize int, err error) {
+// func (app *App) worker(ctx context.Context) {
+// 	for {
+// 		select {
+// 		case <-ctx.Done():
+// 			return
+// 		default:
+// 		}
+// 		job := <-app.jobChan
+// 		func() {
+// 			res := dlResult{}
+// 			defer func() { job.resChan <- res }()
+// 			dlFile, err := app.downloadAndSaveFile(ctx, job.url)
+// 			if err != nil {
+// 				res.error = err
+// 				return
+// 			}
+// 			defer os.Remove(dlFile.name)
+// 			app.Log.Println("file downloaded:", dlFile.name)
+// 			app.Log.Println("uploading file:", dlFile.name)
+// 			msg, err := app.UploadFile(dlFile.name, dlFile.name)
+// 			if err != nil {
+// 				res.error = err
+// 				return
+// 			}
+// 			res.msg = msg
+// 		}()
+// 	}
+// }
+
+func getFileName(resp *http.Response) string {
+	if cd := resp.Header.Get("Content-Disposition"); cd != "" {
+		if _, params, err := mime.ParseMediaType(cd); err == nil {
+			if fname, ok := params["filename"]; ok && fname != "" {
+				return fname
+			}
+		}
+	}
+	if resp.Request != nil && resp.Request.URL != nil {
+		fname := path.Base(resp.Request.URL.Path)
+		if fname != "." && fname != "/" {
+			return fname
+		}
+	}
+	return ""
+}
+
+func getRemoteFileInfo(ctx context.Context, fileUrl string) (fname string, fsize int64, err error) {
 	req, err := http.NewRequestWithContext(ctx, "HEAD", fileUrl, nil)
 	if err != nil {
 		return
@@ -138,68 +238,62 @@ func getRemoteFileInfo(ctx context.Context, fileUrl string) (fname string, fsize
 	}
 	defer resp.Body.Close()
 
-	fsize, err = strconv.Atoi(resp.Header.Get("Content-Length"))
+	fsize, err = strconv.ParseInt(resp.Header.Get("Content-Length"), 10, 64)
 	if err != nil {
 		return
 	}
 
-	fname = getFilename(resp)
+	fname = getFileName(resp)
 	return
 }
 
-func (app *App) downloadAndSaveFile(ctx context.Context, fileUrl string) (*dlFile, error) {
-	fname, fsize, err := getRemoteFileInfo(ctx, fileUrl)
-	if err != nil {
-		return nil, err
-	}
-	if fname == "" {
-		return nil, errors.New("could not get file name")
-	}
-	if fsize > maxFileSize {
-		return nil, errors.New("file size is bigger than max file size")
-	}
-
-	req, err := http.NewRequestWithContext(ctx, "GET", fileUrl, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	app.Log.Printf("downloading file (%dKB): %s\n", fsize/1024, fname)
-	f, err := os.Create(fname)
-	if err != nil {
-		return nil, err
-	}
-
-	ioCtx, ioCancel := context.WithTimeout(ctx, time.Minute * 60)
-	defer ioCancel()
-
-	done := make(chan error)
-	go func() {
-		var n   int64
-		var err error
-		n, err = io.Copy(f, resp.Body)
-		if n != int64(fsize) {
-			err = errors.New("file does not downloaded properly")
-		}
-		done <- err
-	}()
-
-	select {
-	case err := <-done:
-		if err != nil {
-			return nil, err
-		}
-		return &dlFile{name: fname, size: fsize}, nil
-	case <-ctx.Done():
-		return nil, ioCtx.Err()
-	}
-}
+// func (app *App) downloadAndSaveFile(ctx context.Context, fileUrl string) (*dlFile, error) {
+// 	fname, fsize, err := getRemoteFileInfo(ctx, fileUrl)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	if fname == "" {
+// 		return nil, ErrEmptyFileName
+// 	}
+// 	if fsize > maxFileSize {
+// 		return nil, ErrMaxFileSize
+// 	}
+// 	req, err := http.NewRequestWithContext(ctx, "GET", fileUrl, nil)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	resp, err := http.DefaultClient.Do(req)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer resp.Body.Close()
+// 	app.Log.Printf("downloading file (%dKB): %s\n", fsize/1024, fname)
+// 	f, err := os.Create(fname)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	ioCtx, ioCancel := context.WithTimeout(ctx, time.Minute*60)
+// 	defer ioCancel()
+// 	done := make(chan error)
+// 	go func() {
+// 		var n int64
+// 		var err error
+// 		n, err = io.Copy(f, resp.Body)
+// 		if n != fsize {
+// 			err = errors.New("file does not downloaded properly")
+// 		}
+// 		done <- err
+// 	}()
+// 	select {
+// 	case err := <-done:
+// 		if err != nil {
+// 			return nil, err
+// 		}
+// 		return &dlFile{name: fname, size: fsize}, nil
+// 	case <-ctx.Done():
+// 		return nil, ioCtx.Err()
+// 	}
+// }
 
 func (app *App) InitBot(ctx context.Context) error {
 	botHost := utils.GetNonEmptyEnv(hostEnvVar)

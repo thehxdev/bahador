55c7cbdef7989d6e0ed333f9e9ac898cc96119a7
thehxdev <hossein.khosravi.ce@gmail.com>
2025-09-09 13:50:47 +0330

    rename min and max parameters to low and high
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- utils/random.go
+++ utils/random.go
@@ -5,8 +5,8 @@ import (
 	"math/big"
 )
 
-func GenRandInt64(min, max int64) (int64, error) {
-	delta := max - min + 1
+func GenRandInt64(low, high int64) (int64, error) {
+	delta := high - low + 1
 	n := big.NewInt(delta)
 	randNum, err := rand.Int(rand.Reader, n)
 	if err != nil {
